6d8d5f8cb1bc5adaf0524662d1b59bf1c8227d1d
Akhil Majety <93578655+Zephyr1080@users.noreply.github.com>
2026-08-24 22:16:56 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            seen = set()
+            for row in range(9):
+                val = board[row][i]
+                if val == ".":
+                    continue
+                if val in seen:
+                    return False
+                seen.add(val)
+
+        for i in range(9):
+            seen = set()
+            for col in range(9):
+                val = board[i][col]
+                if val == ".":
+                    continue
+                if val in seen:
+                    return False
+                seen.add(val)
+
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square // 3) * 3 + i
+                    col = (square % 3) * 3 + j
+                    val = board[row][col]
+                    if val == ".":
+                        continue
+                    if val in seen:
+                        return False
+                    seen.add(val)
+
+        return True
